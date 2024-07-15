5a884f483be897bd5fbeda43b1ebf0ef0712b5cf
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2024-07-15 22:50:16 +1000

    use asyncio for multi client handling

--- server.py
+++ server.py
@@ -1,65 +1,35 @@
-import socket
+import asyncio
 import config
 
 import logging
 logging.basicConfig(format='%(asctime)s: %(message)s', level=logging.INFO, datefmt='%H:%M:%S')
 
-import threading
-import time
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('0.0.0.0', config.PORT))
-server.listen(5)
-
-logging.info(f'server listening on {config.HOST}:{config.PORT}.')
-
-# connection handler
-class ConnHandler:
-    def __init__(self, conn: socket.socket):
-        self.conn = conn
-        self.name = str(conn.getsockname())
-        logging.info(f'{self.name}: new connection')
-
-        conn.setblocking(False)
-
-    def __del__(self):
-        logging.info(f'{self.name}: destroying object')
-        self.conn.close() # close connection - the handler will exit by itself (hopefully)
-
-    def run(self):
-        self.handler_thread = threading.Thread(target=self.handler)
-        self.handler_thread.run()
-
-    def handler(self):
-        while True:
-            try:
-                msg = self.conn.recv(2048).decode('utf-8')
-            except BlockingIOError:
-                time.sleep(0.001)
-                continue
-            if len(msg) == 0:
-                # continue
-                logging.info(f'{self.name}: client disconnected')
-                return
-            else:
-                logging.info(f'{self.name}: received message: {msg}')
-                if msg == config.SERVER_DISCONNECT_MSG:
-                    logging.info(f'{self.name}: requested disconnect')
-                    self.conn.close()
-                    return
-
-# loop for dispatching connection handler threads
-handlers = []
-def conn_loop():
+async def client_task(reader, writer):
+    name = writer.get_extra_info('peername')
     while True:
-        conn, addr = server.accept() # wait until new client arrives
-        handlers.append(ConnHandler(conn))
-        handlers[-1].run()
-threading.Thread(target=conn_loop).run() # launch loop in a different thread so we can listen for Ctrl-C
+        msg = (await reader.read(2048)).decode('utf-8')
+        if len(msg) == 0:
+            # continue
+            logging.info(f'{name}: client disconnected')
+            return
+        else:
+            logging.info(f'{name}: received message: {msg}')
+            if msg == config.SERVER_DISCONNECT_MSG:
+                logging.info(f'{name}: requested disconnect')
+                return
+
+def client_connected_cb(reader, writer):
+    logging.info(f'{writer.get_extra_info('peername')}: client connected')
+    asyncio.ensure_future(client_task(reader, writer))
+
+loop = asyncio.get_event_loop()
+server_coro = asyncio.start_server(client_connected_cb, host = config.BIND_HOST, port = config.PORT)
+server = loop.run_until_complete(server_coro)
 
 try:
-    while True:
-        pass
+    logging.info(f'server listening on {config.HOST}:{config.PORT}.')
+    loop.run_forever()
 finally:
-    for h in handlers:
-        del h
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
